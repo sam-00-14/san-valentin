b8cdbf2b3c2695a2c47532f59ff34fcbdee442c7
SAM-00-14 <65gtdavid65@gmail.com>
2026-02-14 19:01:43 -0500

    San Valentin

---
+++ public/script.js
@@ -0,0 +1,42 @@
+const API = "";
+
+function mostrarSorpresa(){
+    document.getElementById("sorpresa").style.display="block";
+}
+
+async function enviarMensaje(){
+
+    const nombre = document.getElementById("nombre").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    await fetch(API + "/mensaje", {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({ nombre, mensaje })
+    });
+
+    cargarMensajes();
+}
+
+async function cargarMensajes(){
+
+    const res = await fetch(API + "/mensajes");
+    const data = await res.json();
+
+    const cont = document.getElementById("listaMensajes");
+
+    cont.innerHTML="";
+
+    data.reverse().forEach(m => {
+        cont.innerHTML += `
+        <div class="mensaje">
+        <b>${m.nombre}</b><br>
+        ${m.mensaje}
+        </div>
+        `;
+    });
+}
+
+cargarMensajes();
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const DB_FILE = "mensajes.json";
+
+// Crear archivo si no existe
+if (!fs.existsSync(DB_FILE)) {
+    fs.writeFileSync(DB_FILE, "[]");
+}
+
+// GUARDAR MENSAJE
+app.post("/mensaje", (req, res) => {
+
+    const { nombre, mensaje } = req.body;
+
+    if (!nombre || !mensaje) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+
+    const data = JSON.parse(fs.readFileSync(DB_FILE));
+
+    data.push({
+        nombre,
+        mensaje,
+        fecha: new Date()
+    });
+
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+
+    res.json({ ok: true });
+});
+
+// OBTENER MENSAJES
+app.get("/mensajes", (req, res) => {
+
+    const data = JSON.parse(fs.readFileSync(DB_FILE));
+    res.json(data);
+
+});
+
+app.listen(3000, () => {
+    console.log("Servidor listo http://localhost:3000");
+});
